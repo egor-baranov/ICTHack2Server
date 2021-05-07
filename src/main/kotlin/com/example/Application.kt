d73dc8b798dad2d1db58dbb657300b8319452a00
Daza10samu <andreyhigher@gmail.com>
2021-05-07 20:59:35 +0300

    🐛 added configurations

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -1,13 +1,21 @@
 package com.example
 
+import com.example.plugins.configureRouting
+import com.example.plugins.configureSecurity
+import com.example.plugins.configureSerialization
+import com.example.routes.configureProject
+import com.example.routes.configureReply
+import com.example.routes.configureUser
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
-import com.example.plugins.*
 
 fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
         configureRouting()
         configureSecurity()
         configureSerialization()
+        configureUser()
+        configureProject()
+        configureReply()
     }.start(wait = true)
 }
